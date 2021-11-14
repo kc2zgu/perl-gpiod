8675984f83faef8f6fa0f54be9f4721404d2f639
Stephen Cavilia <sac@atomicradi.us>
2021-11-14 12:59:56 -0500

    Alien::libgpiod module v0.1

---
+++ Alien-libgpiod/lib/Alien/libgpiod.pm
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+package Alien::libgpiod;
+
+use parent qw(Alien::Base);
+
+our $VERSION = 'v0.1';
+
+# ABSTRACT: Alien module for libgpiod (Linux GPIO character device library)
+
+1;
